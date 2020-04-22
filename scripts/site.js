bbf64a4de22e9c138949354a83b85646813de747
Jorge Eliecer <jorge.m.h@outlook.com>
2020-04-22 17:54:14 -0500

    Tagged template literals

--- scripts/site.js
+++ scripts/site.js
@@ -109,7 +109,7 @@ function loadApplication() {
 
   var riskLabel = document.getElementById("riskSummary");
   riskLabel.style.display = "block";
-  riskLabel.innerText = generateRickProfile(findLoanApp); 
+  riskLabel.innerHTML = generateRickProfile(findLoanApp); 
   }
 }
 
@@ -353,9 +353,34 @@ function generateRickProfile(la) {
       riskProfile = "high";
   }
 
-  var summaryText = `Dear ${la.ApplicantName}, 
-  your application for ${"$" + la.LoanAmount}, ${reviewText}.
-  Your risk profile is ${riskProfile}`;
+  var applicationCode = String.raw `\t${createApplicationId()}`;
+
+  var summaryText = highlighText `Dear ${la.ApplicantName}, <br>
+  your application for ${"$" + la.LoanAmount}, ${reviewText}. <br>
+  Your risk profile is ${riskProfile} <br>
+  Your unique application code is ${applicationCode}`;
 
   return summaryText;
+}
+
+function highlighText(strings, ...values) {
+  let str = "";
+  for (var i = 0; i < strings.raw.length; i ++) {
+    if (i > 0) {
+      str += `<b>${values[i-1]}</b>`
+    }
+    str += strings.raw[i];
+  }
+  return str;
+}
+
+function createApplicationId() {
+  var result = '';
+  var characters = "ABCDEEUVYZabcdrswxyz01789/\\#@$%()*^!'";
+  var charactersLength = characters.length;
+  for (var i = 0; i < 8; i ++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
 }
